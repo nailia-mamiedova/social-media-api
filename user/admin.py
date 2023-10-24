4a64bb4f227ca05151668488496b5cec57871298
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-24 18:21:26 +0300

    Register User in admin & Create initial serializer

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "username", "first_name", "last_name", "is_staff")
+    search_fields = ("username", "first_name", "last_name")
+    ordering = ("email",)
