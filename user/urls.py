ce94037d5fc2fe4c73e569ae75e685da76c3f44d
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-24 18:25:17 +0300

    Add views for manage and create user & Add routes

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = []
+from django.urls import path
+
+from user.views import CreateUserView, ManageUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
 
 app_name = "user"
