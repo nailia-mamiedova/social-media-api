da886c83e9550c9da105d2e533fa7a5bebcbcf22
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-10-24 19:40:26 +0300

    Create UserViewSet for list and detail views & Add routes to urls

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from user.views import CreateUserView, ManageUserView
+from user.views import CreateUserView, ManageUserView, UserViewSet
+
+route = routers.DefaultRouter()
+route.register("users", UserViewSet)
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("me/", ManageUserView.as_view(), name="manage"),
+    path("", include(route.urls)),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,11 +1,27 @@
-from rest_framework import generics
+from rest_framework import generics, mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from user.serializers import UserSerializer
+from user.models import User
+from user.serializers import UserSerializer, UserCreateSerializer, UserDetailSerializer
+
+
+class UserViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return UserDetailSerializer
+
+        return UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserCreateSerializer
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
